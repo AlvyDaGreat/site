62ac27cba56fad87000cf2da94d929ef1b21c5b3
AlvyDaGreat <51297085+AlvyDaGreat@users.noreply.github.com>
2021-04-28 11:39:52 -0500

    ok fixed it i think

--- random-meme/random-redirect.js
+++ random-meme/random-redirect.js
@@ -1,8 +1,5 @@
-jQuery.get('./memes.txt', function(data) {
-	sites = data.split('\n');
+sites = ['https://streamable.com/lzr4ix', 'https://disq.me/6ZjUY.wav'];
 
-	var i = Math.round(Math.random()*(sites.length-1));
+var i = Math.round(Math.random()*(sites.length-1));
 
-	window.location.href=sites[i];
-
-}, 'text');
+window.location.href=sites[i];
