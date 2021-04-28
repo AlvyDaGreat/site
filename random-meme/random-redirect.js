fef5b0c0c66ccdb8061a42ffef1e99e5a3ba5e80
AlvyDaGreat <51297085+AlvyDaGreat@users.noreply.github.com>
2021-04-28 11:36:11 -0500

    make it use a discord file

--- random-meme/random-redirect.js
+++ random-meme/random-redirect.js
@@ -1,4 +1,4 @@
-jQuery.get('./text.txt', function(data) {
+jQuery.get('https://cdn.discordapp.com/attachments/572536078536278056/837004174209318992/memes.txt', function(data) {
 	sites = data.split('\n');
 
 	var i = Math.round(Math.random()*(sites.length-1));
