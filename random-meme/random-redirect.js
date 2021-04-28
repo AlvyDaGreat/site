eb3478eb649ad3758ac2cdc7610b2f574da5f925
AlvyDaGreat <51297085+AlvyDaGreat@users.noreply.github.com>
2021-04-28 11:40:54 -0500

    wait frick go back

--- random-meme/random-redirect.js
+++ random-meme/random-redirect.js
@@ -1,5 +1,8 @@
-sites = ['https://streamable.com/lzr4ix', 'https://disq.me/6ZjUY.wav'];
+jQuery.get('./memes.txt', function(data) {
+    sites = data.split('\n');
 
-var i = Math.round(Math.random()*(sites.length-1));
+    var i = Math.round(Math.random()*(sites.length-1));
 
-window.location.href=sites[i];
+    window.location.href=sites[i];
+
+}, 'text');
