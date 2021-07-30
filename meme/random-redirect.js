a8d0e68275b72beee3003e886b0060736687fdaa
AlvyDaGreat <51297085+AlvyDaGreat@users.noreply.github.com>
2021-07-30 12:16:53 -0500

    Delete meme directory

--- meme/random-redirect.js
+++
@@ -1,8 +0,0 @@
-jQuery.get('./memes.txt', function(data) {
-    sites = data.split('\n');
-
-    var i = Math.round(Math.random()*(sites.length-1));
-
-    window.location.href=sites[i];
-
-}, 'text');
